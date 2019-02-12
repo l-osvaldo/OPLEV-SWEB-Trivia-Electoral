7c646a86f290ff55a9c71504cc05dc05f5a94f63
Yesi Days <silvercorp@gmail.com>
2019-02-12 00:42:28 -0600

    Vista + Plantilla + Assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,20 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      'assets': path.resolve('resources/assets')
+    }
+  }
+});
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.copyDirectory('resources/assets/css', 'public/css');
+mix.copyDirectory('resources/assets/images', 'public/images');
+mix.copyDirectory('resources/assets/vendor_components', 'public/vendor_components');
+mix.copyDirectory('resources/assets/vendor_plugins', 'public/vendor_plugins');
+mix.copyDirectory('resources/assets/js', 'public/js');
+
