8878980db1184acae370c8133aa7856b49f92992
vagrant <vagrant@homestead>
2019-02-26 10:19:25 +0000

    se termino reporte de indicadores

--- resources/views/pages/reportes/indicadores.blade.php
+++ resources/views/pages/reportes/indicadores.blade.php
@@ -28,15 +28,6 @@
         margin: 53mm 8mm 6mm 10mm;
       }
 
-      .tablacontenido
-      {        
-        border: .5px solid;
-      }
-
-      .datos
-      {
-        font-size: 11px;
-      }
 
       .colinfo
       {
@@ -45,6 +36,20 @@
         border-bottom: .5px solid;
       }
 
+
+      .info
+      {
+        border: .5px solid;
+        font-size: 12px;
+        text-align: center;
+      }
+
+      .infoobserva
+      {
+        border: .5px solid;
+        font-size: 12px;
+        margin-left: 10px;
+      }
 
       table tr.rubro{ background: #bfbfbf; border:1px solid #46453f; }
       table tr.rubro td{ text-align: center;  font-weight:900; height: 30px; font-size: 10px; border:1px solid #46453f;}
@@ -89,28 +94,95 @@
 
     <table>
       <tr class="rubro">
-        <td width="20%">Programa Presupuestario</td>        
+        <td width="20%" rowspan="2">Programa Presupuestario</td>        
         <td class="texto">01. Desarrollo y Fortalecimiento Institucional</td>        
         <td class="texto">02. Proceso Electoral</td>        
         <td class="texto">03. Cartera de Proyectos</td>
         <td class="texto">04. Prerrogativas y Partidos Políticos</td>
       </tr>
+      <tr class="rubro">        
+        <td class="texto">{{ $indicadores['prog1'] }}</td>        
+        <td class="texto">{{ $indicadores['prog2'] }}</td>        
+        <td class="texto">{{ $indicadores['prog3'] }}</td>
+        <td class="texto">{{ $indicadores['prog4'] }}</td>
+      </tr>
+
     </table>
 
     <table>
       <tr class="rubro">
         <td width="20%">Objetivo del Indicador</td>        
-        <td class="texto">Descripción del Objetivo</td>        
+        <td class="texto">{{ $indicadores['objetivoindicador'] }}</td>        
       </tr>
     </table>
 
     <table>
       <tr class="rubro">
         <td width="20%">Periodo de Cumplimiento</td>        
-        <td class="texto">periodo</td>        
+        <td class="texto">{{ $indicadores['abreviaturaperiodocump'] }}</td>        
         <td width="20%" class="rubro">Periodo de Evauación</td>        
-        <td class="texto">mes</td>
+        <td class="texto">{{ $indicadores['mes'] }}</td>
       </tr>
+    </table>
+
+    <table>
+      <tr class="rubro">
+        <td>Clave del Indicador</td>        
+        <td>Nombre del Indicador</td>
+        <td>Frecuencia</td>
+        <td>Variables</td>
+        <td>Meta</td>
+        <td>Realizado vs Programado</td>
+        <td>Resultado</td>
+        <td>Variación</td>
+      </tr>       
+    </table>
+
+    <table>
+      <tr>
+        <td class="info" rowspan="2"  height="70"></td>        
+        <td class="info" rowspan="2">{{ $indicadores['nombreindicador'] }}</td>
+        <td class="info" rowspan="2">{{ $indicadores['frecuencia'] }}</td>
+        <td class="info">{{ $indicadores['variable1'] }}</td>
+        <td class="info" rowspan="2">{{ $indicadores['meta'] }}</td>
+        <td class="info">{{ $indicadores['realizado'] }}</td>
+        <td class="info" rowspan="2">{{ $indicadores['resultado'] }} %</td>
+        <td class="info" rowspan="2">{{ $indicadores['variacion'] }} %</td>
+      </tr>      
+      <tr>        
+        <td class="info">{{ $indicadores['variable2'] }}</td>        
+        <td class="info">{{ $indicadores['meta'] }}</td>
+      </tr>          
+    </table>
+
+    <table>
+      <tr class="rubro">
+        <td>Observaciones</td>
+      </tr>
+    </table>
+
+    <table>
+      <tr>        
+        <td class="infoobserva" height="100">{{ $indicadores['observaciones'] }}<</td>
+      </tr>          
+    </table>
+
+    <table>
+      <tr class="rubro">
+        <td>Responsable de la Información</td>
+      </tr>
+    </table>
+
+    <table>
+      <tr class="rubro">
+        <td>Nombre, cargo y firma del Titular de la Unidad Responsable</td>
+      </tr>
+    </table>
+
+    <table>
+      <tr>        
+        <td class="info" height="80"><br>{{ $indicadores['nombretitular'] }}<br>{{ $indicadores['cargo'] }}</td>
+      </tr>          
     </table>
 
 
