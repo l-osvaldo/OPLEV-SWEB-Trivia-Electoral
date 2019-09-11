2ef5aa621340e91557f1167a387738c7d984f24e
aligutman1 <aligutman1@gmail.com>
2019-09-11 16:39:45 +0000

    bugs pdf produccion

--- resources/views/pages/reportes/trimestrales.blade.php
+++ resources/views/pages/reportes/trimestrales.blade.php
@@ -31,19 +31,19 @@
     <tr>
       <td rowspan="3" style="text-align: center;" width="8%"><img class="logo" src="{{ asset('images/logoople.png') }}" width="100"></td>
       <td class="gris" width="8%">Programa</td>      
-      <td width="35%">  </td>
-      <td class="gris" style="text-align: center;" width="10%">Unidad Responsable<br><span></span></td>
+      <td width="35%"> {{$trimestral[0]['descprograma']}} </td>
+      <td class="gris" style="text-align: center;" width="10%">Unidad Responsable<br><span>{{ $trimestral[0]['nombrearea'] }}</span></td>
     </tr>
 
     <tr>
       
       <td width="8%" class="gris">Programa Específico</td>
-      <td width="35%"></td>
-      <td width="10%" class="gris" style="text-align: center;">Periodo Trimestral<br><span></span></td>
+      <td width="35%">{{ $trimestral[0]['descprogramaesp'] }}</td>
+      <td width="10%" class="gris" style="text-align: center;">Periodo Trimestral<br><span>{{ $trimestral[0]['periodotrimestral'] }}</span></td>
     </tr>
     <tr>
       <td width="8%" class="gris">Objetivo</td>
-      <td colspan="2" width="60%"></td>
+      <td colspan="2" width="60%">{{ $trimestral[0]['objprogramaesp'] }}</td>
     </tr>
   </table>
   <table class="table" border="1" align="center" cellpadding="5px" style="margin-top: -1px;">
@@ -72,7 +72,64 @@
     <td width="5%" style="text-align: center;">%</td>
   </tr>
 
+        @foreach ($trimestral as $indexKey => $trim)      
+          <tr>        
+            <td style="text-align: center;">{{$indexKey+1}}</td>
+            <td style="text-align: justify;padding:3px;">{{ $trim->descactividad }}</td>   
+            <td style="text-align: center;">{{ $trim->unidadmedida }}</td>   
+            <td style="text-align: center;">{{ $trim->cantidadanual }}</td>   
+            <td style="text-align: center;">{{ $trim->inicio }} - {{ $trim->termino }} </td>   
+            <td style="text-align: center;">{{ $trim->avtprogramado }}</td>   
+            <td style="text-align: center;">{{ $trim->avtrealizado }}</td>   
+            <td style="text-align: center;">{{ $trim->avtvariacion }}</td>   
+            <td style="text-align: center;">{{ $trim->avaprogramado }}</td>   
+            <td style="text-align: center;">{{ $trim->avarealizado }}</td>   
+            <td style="text-align: center;">{{ $trim->avacantidad }}</td>   
+            <td style="text-align: center;">{{ $trim->avaporcentaje }}</td>
+            <td style="text-align: left;">{{ $trim->observatrim }}</td>              
+          </tr>
+        @endforeach
 
+
+      @if (($trim->idarea==3) && ($trim->idprograma==4))
+
+          <tr>
+            <td colspan="13" class="gris">
+              LAS FIRMAS AL CALCE AMPARAN LA TOTALIDAD DEL PRESENTE DOCUMENTO
+            </td>
+          </tr>
+
+          <tr style="height: 150px;">
+            <td colspan="6">
+              ELABORÓ<br>MTRA. PATRICIA PÉREZ HERNÁNDEZ<br>TITULAR DE LA UNIDAD TÉCNICA DE PLANEACIÓN
+            </td>
+            <td colspan="7">
+              Vo. Bo.<br>MTRO. HUGO ENRIQUE CASTRO BERNABE<br>SECRETARIO EJECUTIVO
+            </td>
+          </tr>
+       
+      @endif
+
+
+
+      @if ( ($trim->idarea==18) && ($trim->idprograma==1) && ($trim->idprogramaesp==54) )        
+           
+          <tr>
+            <td colspan="13" class="gris">
+              LAS FIRMAS AL CALCE AMPARAN LA TOTALIDAD DEL PRESENTE DOCUMENTO
+            </td>
+          </tr>
+
+          <tr style="height: 150px;">
+            <td colspan="6">
+              ELABORÓ<br>MTRA. PATRICIA PÉREZ HERNÁNDEZ<br>TITULAR DE LA UNIDAD TÉCNICA DE PLANEACIÓN
+            </td>
+            <td colspan="7">
+              Vo. Bo.<br>MTRO. HUGO ENRIQUE CASTRO BERNABE<br>SECRETARIO EJECUTIVO
+            </td>
+          </tr>
+
+      @endif
 
        </table>
 
