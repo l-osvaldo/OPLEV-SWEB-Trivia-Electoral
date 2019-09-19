fe3d803ba4162d96a21feb22eedf2a04c0982b16
aligutman1 <aligutman1@gmail.com>
2019-09-19 18:19:17 +0000

    email

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('creducindo'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('Opl3woProc355'),
 
     /*
     |--------------------------------------------------------------------------
