1d9196d89e27a50b9153954edebef26f6b76a763
aligutman1 <aligutman1@gmail.com>
2019-10-01 17:30:28 +0000

    notificacion mensual

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -50,7 +50,7 @@ class MailController extends Controller
             $objDemo->sender = $userName;
             $objDemo->receiver = $userName;
      
-            Mail::to("contacto.odes@oplever.org.mx")->send(new DemoEmail($objDemo));
+            //Mail::to("contacto.odes@oplever.org.mx")->send(new DemoEmail($objDemo));
 
 
 
@@ -91,7 +91,7 @@ $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
 //mail($para, $titulo, $mensaje, $headers);
 
-//mail($to, $subject, $message, $headers);
+mail($to, $subject, $message, $headers);
 
 
             Alert::success('', 'Notificación mensual enviada')->autoclose(3500);
