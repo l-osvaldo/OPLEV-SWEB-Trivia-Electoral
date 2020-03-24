55366f37402eb0ecdc506fa15261fe0aae3f5a47
vagrant <vagrant@homestead>
2020-03-24 23:52:39 +0000

    depura los widgets

--- webpack.mix.js
+++ webpack.mix.js
@@ -32,7 +32,6 @@ const mix = require('laravel-mix');
  mix.js('resources/js/app.js', 'public/js')
  .sass('resources/sass/app.scss', 'public/css');
 
- mix.copyDirectory('resources/assets/dist', 'public/dist');
  mix.copyDirectory('resources/assets/css', 'public/css');
  mix.copyDirectory('resources/assets/images', 'public/images');
  mix.copyDirectory('resources/assets/js', 'public/js');
