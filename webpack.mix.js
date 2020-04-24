f825b6207b0c52aaf9fcee5a57406f482c042b6e
Harry Jackson <upwardbinkie@gmail.com>
2020-04-24 14:51:33 -0500

    Fix: Quitar vendor_components y vendor_plugins de webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -46,9 +46,6 @@ const mix = require('laravel-mix');
  mix.js('resources/js/app.js', 'public/js')
  .sass('resources/sass/app.scss', 'public/css');
 
- mix.copyDirectory('resources/assets/docs', 'public/docs');
  mix.copyDirectory('resources/assets/css', 'public/css');
  mix.copyDirectory('resources/assets/images', 'public/images');
  mix.copyDirectory('resources/assets/js', 'public/js');
- mix.copyDirectory('resources/assets/vendor_components', 'public/vendor_components');
- mix.copyDirectory('resources/assets/vendor_plugins', 'public/vendor_plugins');
