d3bf3a7912aabb22e421e40dbfcc773998ceb56f
mariana <mariana.guiot@oplever.org.mx>
2020-05-26 02:21:06 +0000

    repara el bug de los botones con la clase input-group-append

--- resources/views/verificador.blade.php
+++ resources/views/verificador.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+<script src='https://www.google.com/recaptcha/api.js'></script>
 
 <section class="content-header pt-5">
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>Formularios</h1>
+        <h1>Verificador de Sellos Digitales del OPLE</h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="#">Inicio</a></li>
-          <li class="breadcrumb-item active">Formularios</li>
+          <li class="breadcrumb-item active">Verificador</li>
         </ol>
       </div>
     </div>
@@ -26,35 +27,23 @@
         <!-- general form elements -->
         <div class="card card-primary card-outline">
           <div class="card-header o-fondo-5">
-            <h3 class="card-title">Formulario ejemplo</h3>
+            <h3 class="card-title">Verificación de sellos digitales</h3>
           </div>
           <!-- /.card-header -->
           <!-- form start -->
           <form role="form">
             <div class="card-body">
               <div class="form-group">
-                <label for="exampleInputEmail1" class="o-form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                <label for="exampleInputEmail1" class="o-form-label">IDENTIFICADOR ÚNICO DEL EMISOR</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="000000L18740145">
               </div>
               <div class="form-group">
-                <label for="exampleInputPassword1" class="o-form-label">Contraseña</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+                <label for="exampleInputPassword1" class="o-form-label">RFC DEL EMISOR</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="OPLE567834DD1">
               </div>
               <div class="form-group">
-                <label for="exampleInputFile" class="o-form-label">Adjuntar PDF</label>
-                <div class="input-group">
-                  <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="exampleInputFile">
-                    <label class="custom-file-label" for="exampleInputFile">PDF</label>
-                  </div>
-                  <div class="input-group-append">
-                    <span class="input-group-text o-fondo-1" id="">Subir</span>
-                  </div>
-                </div>
-              </div>
-              <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Permanecer con la sesión activa</label>
+                <label for="exampleInputPassword1" class="o-form-label">FOLIO INTERNO</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder=" Zm5zSUh-VOUxE-ODNZS05S-TDV4V2-ZyUT09">
               </div>
             </div>
             <!-- /.card-body -->
@@ -67,4 +56,5 @@
         <!-- /.card -->
       </div>
   </section>
+
 @endsection
