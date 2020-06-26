f1b197882a310be95408f726e532bcc73334e56a
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2020-06-26 17:29:05 +0000

    Validación de email

--- app/Http/Controllers/WS/WSAppUsersController.php
+++ app/Http/Controllers/WS/WSAppUsersController.php
@@ -10,15 +10,25 @@ class WSAppUsersController extends Controller
 {
     public function registrarUsuarioApp(Request $request)
     {
-        $usuario            = new AppUser();
-        $usuario->nombre    = $request->nombre;
-        $usuario->email     = $request->email;
-        $usuario->edad      = $request->edad;
-        $usuario->sexo      = $request->sexo;
-        $usuario->municipio = $request->municipio;
-        $usuario->password  = bcrypt($request->password);
-        $usuario->save();
+        $ExisteEmail = AppUser::where('email', $request->email)->count();
 
-        return response()->json($usuario);
+        if ($ExisteEmail > 0) {
+
+            $data = array('done' => false, 'message' => "El email ya esta registrado", 'id' => 0);
+
+        } else {
+            $usuario            = new AppUser();
+            $usuario->nombre    = $request->nombre;
+            $usuario->email     = $request->email;
+            $usuario->edad      = $request->edad;
+            $usuario->sexo      = $request->sexo;
+            $usuario->municipio = $request->municipio;
+            $usuario->password  = bcrypt($request->password);
+            $usuario->save();
+
+            $data = array('done' => true, 'message' => "Usuario Registrado", 'id' => $usuario->id);
+        }
+
+        return response()->json($data);
     }
 }
