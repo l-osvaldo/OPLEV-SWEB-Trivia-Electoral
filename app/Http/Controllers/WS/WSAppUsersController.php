4b9f733f66c21b1f29096e0df214a89f9dda56f5
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2020-07-02 14:39:07 +0000

    cambio en el WS de allPreguntas

--- app/Http/Controllers/WS/WSAppUsersController.php
+++ app/Http/Controllers/WS/WSAppUsersController.php
@@ -56,7 +56,7 @@ class WSAppUsersController extends Controller
 
     public function allPreguntas()
     {
-        $allPreguntas = Pregunta::all();
+        $allPreguntas = Pregunta::select('id', 'pregunta', 'opcion_a', 'opcion_b', 'opcion_c', 'opcion_d', 'respuesta')->get();
         return response()->json($allPreguntas);
     }
 }
